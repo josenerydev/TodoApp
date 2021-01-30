f176cd97b652178c21219857f6d0b65c8a8d342b
josenerydev <josenerydev@gmail.com>
2021-01-29 21:38:45 -0300

    add build with migration

---
+++ Migrations/Scripts/Script0001.sql
@@ -0,0 +1,7 @@
+﻿create table TodoItem
+(
+    Id         bigint primary key identity (1,1),
+    Name       nvarchar(50) not null,
+    IsComplete bit          not null,
+    Secret     nvarchar(50) not null
+);
